471a92910517b698bee1c749b78a4463c1bae57e
Matthew Casey <matthew.casey@d-cat.co.uk>
2022-03-08 12:42:17 +0000

    Remove Python files from documentation

--- docs/common/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This sub-module contains common code for the SDK.
-
-&copy; [Digital Content Analysis Technology Ltd](https://www.d-cat.co.uk)
-"""
--- docs/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This sub-module contains the data models for the SDK.
-
-&copy; [Digital Content Analysis Technology Ltd](https://www.d-cat.co.uk)
-"""
