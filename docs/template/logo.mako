29dd759f2ed86232226f050e086d084f1b278a8c
Matthew Casey <matthew.casey@d-cat.co.uk>
2024-06-11 08:58:12 +0100

    Latest release

--- docs/template/logo.mako
+++ docs/template/logo.mako
@@ -6,7 +6,7 @@
     </h2>
 
     <p>
-        Version: 1.11.1
+        Version: 1.12.2
     </p>
 
     <p>
--- fusion_platform/__init__.py
+++ fusion_platform/__init__.py
@@ -11,8 +11,8 @@ API, and this is defined within the corresponding OpenAPI 3.0 specification, whi
 """
 
 # Do not modify the following two lines as they are maintained by the version.sh script.
-__version__ = '1.11.1'
-__version_date__ = '2024-04-08T10:20:23Z'
+__version__ = '1.12.2'
+__version_date__ = '2024-06-11T07:57:10Z'
 
 # Exclude certain sub-modules from documentation.
 # @formatter:off
